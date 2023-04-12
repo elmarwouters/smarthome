dbb0e5d9d5664271f1a30e33884655d1130a74c4
Elmar Wouters <elmar@elmarwouters.nl>
2023-04-12 18:38:18 +0200

    Very basic with lights turn on turn off

---
+++ components/Lights/Lights.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import React, {useState, useEffect} from 'react';
+import LightsButton from "./LightsButton";
+
+// Define a type for the light data
+type LightData = {
+    state: {
+        on: boolean;
+        bri: number;
+        hue: number;
+        sat: number;
+        effect: string;
+        xy: [number, number];
+        ct: number;
+        alert: string;
+        colormode: string;
+        mode: string;
+        reachable: boolean;
+    };
+    swupdate: {
+        state: string;
+        lastinstall: string;
+    };
+    type: string;
+    name: string;
+    modelid: string;
+    manufacturername: string;
+    productname: string;
+    capabilities: {
+        certified: boolean;
+        control: {
+            mindimlevel: number;
+            maxlumen: number;
+            colorgamuttype: string;
+            colorgamut: [number, number][];
+            ct: {
+                min: number;
+                max: number;
+            };
+        };
+        streaming: {
+            renderer: boolean;
+            proxy: boolean;
+        };
+    };
+    config: {
+        archetype: string;
+        function: string;
+        direction: string;
+        startup: {
+            mode: string;
+            configured: boolean;
+        };
+    };
+    uniqueid: string;
+    swversion: string;
+    swconfigid: string;
+    productid: string;
+};
+
+const Lights = () => {
+    const [lightsData, setLightsData] = useState<Record<string, LightData> | null>(null);
+
+    useEffect(() => {
+        const fetchLightsData = async () => {
+            try {
+                const response = await fetch('/lights');
+                const data = await response.json();
+                setLightsData(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchLightsData().then();
+    }, []);
+
+    return (
+        <div>
+            <h2 className="text-2xl font-bold mb-4">Philips Hue</h2>
+            <div className="rounded-t-xl overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10 w-full">
+                <div className="grid">
+                    <div className="grid grid-cols-4 gap-4">
+                        <div><strong>ID</strong></div>
+                        <div><strong>Name</strong></div>
+                        <div><strong>Type</strong></div>
+                        <div><strong>On/Off</strong></div>
+                    </div>
+                    {lightsData ? (
+                        Object.keys(lightsData).map(lightId => {
+                            const light = lightsData[lightId];
+                            return (
+                                <div className="grid grid-cols-4 gap-4 my-4" key={lightId}>
+                                    <div>{lightId}</div>
+                                    <div>{light.name}</div>
+                                    <div>{light.type}</div>
+                                    <div className="h-1 w-1 flex">
+                                        <LightsButton id={lightId}/>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <p>Loading lights data...</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Lights;
---
+++ components/Lights/LightsButton.tsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const LightsButton = ({id}: { id: string }) => {
+    const [lightLoading, setLightLoading] = useState(true)
+    const [lightState, setLightState] = useState(false)
+
+    useEffect(() => {
+        checkLightState(id).then((res) => {
+            if (res && res.data.state.on) {
+                setLightState(true)
+            } else {
+                setLightState(false)
+            }
+
+            setLightLoading(false);
+        })
+    });
+
+    const turnLightOnOrOff = async (id: string, onOff: boolean) => {
+        const url = `http://${process.env.NEXT_PUBLIC_HUE_IP}/api/${process.env.NEXT_PUBLIC_HUE_USER}/lights/${id}/state`;
+
+        setLightLoading(true);
+
+        try {
+            await axios.put(url, {
+                on: onOff,
+            });
+
+            const lightState = await checkLightState(id);
+
+            if (lightState && lightState.data.state.on) {
+                setLightState(true)
+            } else {
+                setLightState(false)
+            }
+
+            setLightLoading(false);
+
+            return true
+
+        } catch (err) {
+            console.error(err);
+            setLightLoading(false)
+        }
+    };
+
+    const checkLightState = async (id: string) => {
+        const url = `http://${process.env.NEXT_PUBLIC_HUE_IP}/api/${process.env.NEXT_PUBLIC_HUE_USER}/lights/${id}`;
+
+        try {
+            return await axios.get(url);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <>
+            {!lightLoading && (
+                <button
+                    className={`p-4 rounded-full ${lightState ? `bg-green-500` : `bg-red-700`}`}
+                    onClick={() => turnLightOnOrOff(id, !lightState ? true : false)}
+                />
+            )}
+        </>
+    )
+}
+
+export default LightsButton;
